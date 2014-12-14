740822b5d5da0811eb1f38af393325cadf833195
MauriceTheObese <denchikg@gmail.com>
2014-12-15 00:07:15 +0200

    fixed semanticChecksMoudle
    shachar:
    
    - pick a better name and add documenation to the top of the file
    - run tests
    - i fixed a few things you forgot

--- PA2/src/IC/SemanticChecks/SemanticChecksModul.java
+++ PA2/src/IC/SemanticChecks/SemanticChecksModul.java
@@ -3,15 +3,8 @@ import java.util.List;
 
 import IC.AST.*;
 import IC.SymTables.ClassSymbolTable;
-import IC.SymTables.MethodSymbolTable;
-import IC.SymTables.SymbolTable;
-import IC.SymTables.Symbols.ClassSymbol;
-import IC.SymTables.Symbols.FieldSymbol;
-import IC.SymTables.Symbols.LocalVariableSymbol;
 import IC.SymTables.Symbols.MethodSymbol;
-import IC.SymTables.Symbols.ParameterSymbol;
-import IC.SymTables.Symbols.StaticMethodSymbol;
-import IC.SymTables.Symbols.VirtualMethodSymbol;
+
 import IC.SemanticChecks.SemanticError;
 import IC.Types.ArrayType;
 import IC.Types.MethodType;
@@ -20,367 +13,428 @@ import IC.Types.Type;
 import IC.Types.VoidType;
 
 
+/**
+ * 
+ * TODO: write purpose of this file
+ * run tests
+ *
+ */
 
-public class SemanticChecksModul implements  PropagatingVisitor<Object, Boolean>  {
+public class SemanticChecksModul implements  Visitor  {
 
 	
 	boolean inside_loop = false;
 	boolean inside_static_function = false;
 	boolean found_main = false;
 	
-	public Boolean check(Program program) throws SemanticError
+	public void check(Program program) throws SemanticError
 	{
-		return program.accept(this, null);	
+		 program.accept(this);	
 	}
 	
 	
 	@Override
-	public Boolean visit(Program program, Object context) throws SemanticError {
+	public Object visit(Program program) throws SemanticError {
+		
 		/* iterate over the classes in the program */
 		List<ICClass> classList = program.getClasses();
-		boolean rc;
+		
 		for(ICClass curr_class : classList)
 		{
-			rc = curr_class.accept(this , null);
-			if ( rc == false)
-				return false;
+			curr_class.accept(this );
+			
 		}
 		
-		return true;
-	}
-
-	@Override
-	public Boolean visit(ICClass icClass, Object context) throws SemanticError {
-		List<Method> methods = icClass.getMethods();
-		boolean rc;
-		for(Method method : methods)
-		{
-			rc = method.accept(this, null);
-			if ( rc == false)
-				return false;
-		}
-		
-		return true;
-	}
-
-	@Override
-	public Boolean visit(Field field, Object context) throws SemanticError {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Boolean visit(VirtualMethod method, Object context)
+	public Object visit(ICClass icClass) throws SemanticError {
+		List<Method> methods = icClass.getMethods();
+		
+		/* visit each method */
+		for(Method method : methods)
+		{
+			method.accept(this);
+			
+		}
+		
+		return null;
+	}
+
+	@Override
+	public Object visit(Field field) throws SemanticError {
+		// wont visit fields
+		return null;
+	}
+
+	@Override
+	public Object visit(VirtualMethod method)
 			throws SemanticError {
 		
-		
-		 List<Statement> stList = method.getStatements();
+	
 		
 		/* checking that it's not a main function */
-		if ( method.getName().equals("main") && found_main == true) /* DENIS is that necassetry? */
-		{
-			
-			String err_msg = "Found more than one main functions";
+		if ( method.getName().equals("main") && found_main == true) 
+		{	
+			String err_msg = "Found more than one main function";
 			throw new SemanticError(method.getLine(), err_msg);
 		}
 		
+		List<Statement> stList = method.getStatements();
+		
 		for (Statement stm: stList)
 		{
-			stm.accept(this,null);
+			/* statements may use this keyword */
+			stm.accept(this);
 		}
 		
-		return true;
+		return null;
+		
 	}
 	
 
 
 	@Override
-	public Boolean visit(StaticMethod method, Object context)
+	public Object visit(StaticMethod method)
 			throws SemanticError {
 
 		String methodName = method.getName();
 		List<Statement> stList = method.getStatements();
 		
-		if ( methodName.equals("Main"))
+		if ( methodName.equals("main"))
 		{
-			/* checking that it's in a correct form of a main function */
-			boolean fail = false;
-			ClassSymbolTable scope = (ClassSymbolTable) method.enclosingScope();  // ??
-			MethodSymbol SymbolTable =  scope.getMethod("Main", true );
-			MethodType methodType = (MethodType) SymbolTable.getType();
-			List<Type> arguments = methodType.getArguments();
 			
 			
 			if(found_main == true) /* we have already seen a main function */
 			{
-				fail = true;
+				String err_msg = "Found more than one main functions";
+				throw new SemanticError(method.getLine(), err_msg);
 			}
+			
+			/* checking that it's in a correct form of a main function */
+	
+			ClassSymbolTable scope = (ClassSymbolTable) method.enclosingScope();  
+			MethodSymbol SymbolTable =  scope.getMethod("main", true );
+			MethodType methodType = (MethodType) SymbolTable.getType();
+			List<Type> arguments = methodType.getArguments();
+			
+			
 			if ( ! (methodType.getReturnType() instanceof VoidType) ) /* the return type is not a void */
 			{
-				fail = true;
+				String err_msg = "main method invalid return type, must return void";
+				throw new SemanticError(method.getLine(), err_msg);
 			}
 		
 			if( !( (arguments.size() == 1) && (arguments.get(0) instanceof ArrayType) /* params are string [] */
 					&&   ((ArrayType) arguments.get(0)).getBasicType() instanceof StringType   
-					&&    ((ArrayType) arguments.get(0)).getDimensions() != 1 ))
+					&&    ((ArrayType) arguments.get(0)).getDimensions() == 1 ) )
 			{
-				fail = true;
-			}
-			
-			if (fail == true)
-			{
-				String err_msg = "Main function is not in the corect signature";
+				String err_msg = "main method invalid argument list, should only accept one argument of type string[]";
 				throw new SemanticError(method.getLine(), err_msg);
 			}
+			
+			
 			found_main = true;
 			 
 		}
 		
-		inside_static_function = true;
+		inside_static_function = true; /* mark that this cannot appear as an expression inside this method */
 		for (Statement stm: stList)
 		{
-			stm.accept(this,null);
+			stm.accept(this);
 		}
 		inside_static_function = false;
-		return true;
+		
+		return null;
 	}
 
 	@Override
-	public Boolean visit(LibraryMethod method, Object context)
+	public Object visit(LibraryMethod method)
 			throws SemanticError {
-		method.accept(this,null);
-		return true;
-	}
-
-	@Override
-	public Boolean visit(Formal formal, Object context) throws SemanticError {
-		return true;
-	}
-
-	@Override
-	public Boolean visit(PrimitiveType type, Object context)
-			throws SemanticError {
-		return true;
-	}
-
-	@Override
-	public Boolean visit(UserType type, Object context) throws SemanticError {
-		return true;
-	}
-
-	@Override
-	public Boolean visit(Assignment assignment, Object context)
-			throws SemanticError {
-		assignment.getAssignment().accept(this,null); 
-		return true;
-	}
-
-	@Override
-	public Boolean visit(CallStatement callStatement, Object context)
-			throws SemanticError {
-		callStatement.getCall().accept(this,null);
-		return true;
-	}
-
-	@Override
-	public Boolean visit(Return returnStatement, Object context)
-			throws SemanticError {
-		returnStatement.getValue().accept(this,null);
-		return true;
-	}
-
-	@Override
-	public Boolean visit(If ifStatement, Object context) throws SemanticError {
+		
+		/* not sure exactly what to do here .. is the libabry allowed to have a method with name main ? */
 		
 		
-		ifStatement.getCondition().accept(this,null);
-		ifStatement.getOperation().accept(this,null);
-		ifStatement.getElseOperation().accept(this,null);
+		return null;
+	}
 
+	@Override
+	public Object visit(Formal formal) throws SemanticError {
 		return true;
 	}
 
 	@Override
-	public Boolean visit(While whileStatement, Object context)
+	public Object visit(PrimitiveType type)
 			throws SemanticError {
-		/* we are in a while loop - thus the visit of a break statement is valid */
+		return true;
+	}
+
+	@Override
+	public Object visit(UserType type) throws SemanticError {
+		return true;
+	}
+
+	@Override
+	public Object visit(Assignment assignment)
+			throws SemanticError {
+			
+			
+		assignment.getAssignment().accept(this);
+		
+		assignment.getVariable().accept(this); /* also visit the location expression, may contain this*/
+		
+		return null;
+	}
+
+	@Override
+	public Object visit(CallStatement callStatement)
+			throws SemanticError {
+		
+		callStatement.getCall().accept(this);
+		return null;
+	}
+
+	@Override
+	public Object visit(Return returnStatement)
+			throws SemanticError {
+		
+		if(returnStatement.hasValue()) /* return value is optional */
+			returnStatement.getValue().accept(this);
+		return null;
+	}
+
+	@Override
+	public Object visit(If ifStatement) throws SemanticError {
+		
+		
+		ifStatement.getCondition().accept(this);
+		ifStatement.getOperation().accept(this);
+		if(ifStatement.hasElse()) /* else is optional, may contain null */
+			ifStatement.getElseOperation().accept(this);
+
+		return null;
+	}
+
+	@Override
+	public Object visit(While whileStatement)
+			throws SemanticError {
+			
+		/* visit the condition */
+		whileStatement.getCondition().accept(this); 
+		
+		/* we are in a while loop - thus the visit of a break and continue statements is valid */
 		Statement st = whileStatement.getOperation();
 		
-		if(inside_loop = true) /* we are already inside a loop */
+		if(inside_loop == true) /* we are already inside a loop */
 		{
-			st.accept(this, null);
-			return true;
+			st.accept(this);
 		}
 		else
 		{
 			inside_loop = true;
-			st.accept(this, null);
+			st.accept(this);
 			inside_loop = false;
-			return true;
 		}
+		
+		return null;
 	}
 
 	@Override
-	public Boolean visit(Break breakStatement, Object context)
+	public Object visit(Break breakStatement)
 			throws SemanticError {
 		if( inside_loop == false)
 		{
-			String err_msg = "Found a break not inside a loop statement";
+			String err_msg = "break can only be used when inside a while statement";
 			throw new SemanticError(breakStatement.getLine(), err_msg);
 		}
-		else
-		{
-			return true;
-		}
+
+
+		return null;
 		
 	}
 
 	@Override
-	public Boolean visit(Continue continueStatement, Object context)
+	public Object visit(Continue continueStatement)
 			throws SemanticError {
+		
 		if( inside_loop == false)
 		{
-			String err_msg = "Found a break not inside a loop statement";
+			String err_msg = "break can only be used when inside a while statement";
 			throw new SemanticError(continueStatement.getLine(), err_msg);
 		}
-		else
-		{
-			return true;
-		}
+		
+		
+		return null;
 	}
 
 	@Override
-	public Boolean visit(StatementsBlock statementsBlock, Object context)
+	public Object visit(StatementsBlock statementsBlock)
 			throws SemanticError {
 		 List<Statement> stList = statementsBlock.getStatements();
+		 
 		 for ( Statement st: stList)
 		 {
-			 st.accept(this, null);
+			 st.accept(this);
 		 }
-		return true;
+		 
+		 return null;
 	}
 
 	@Override
-	public Boolean visit(LocalVariable localVariable, Object context)
+	public Object visit(LocalVariable localVariable)
 			throws SemanticError {
-		return true;
+			
+		if(localVariable.hasInitValue())
+		{
+			// may have initial expression
+			localVariable.getInitValue().accept(this);
+		}
+		return null;
 	}
 
 	@Override
-	public Boolean visit(VariableLocation location, Object context)
+	public Object visit(VariableLocation location)
 			throws SemanticError {
-		return true;
+		
+		if(location.isExternal())
+		{
+			/* may have location: expr.a for example */
+			location.getLocation().accept(this);
+		}
+		
+		return null;
 	}
 
 	@Override
-	public Boolean visit(ArrayLocation location, Object context)
+	public Object visit(ArrayLocation location)
 			throws SemanticError {
-		location.getArray().accept(this, null);
-		location.getIndex().accept(this, null);
-		return true;
+		
+		location.getArray().accept(this);
+		location.getIndex().accept(this);
+		return null;
 	}
 
 	@Override
-	public Boolean visit(StaticCall call, Object context) throws SemanticError {
-		 List<Expression> arg = call.getArguments();
-		 for (Expression a: arg)
+	public Object visit(StaticCall call) throws SemanticError {
+		
+		 List<Expression> args = call.getArguments();
+		 for (Expression arg: args)
 		 {
-			 a.accept(this, null);
+			 arg.accept(this);
 		 }
 		
 		
-		return true;
+		return null;
 	}
 
 	@Override
-	public Boolean visit(VirtualCall call, Object context) throws SemanticError {
+	public Object visit(VirtualCall call) throws SemanticError {
+		
+		 if(call.isExternal())
+		 {
+			 /* might have location:  expr.func() */
+			 call.getLocation().accept(this);
+		 }
+		
 		 List<Expression> arg = call.getArguments();
 		 for (Expression a: arg)
 		 {
-			 a.accept(this, null);
+			 a.accept(this);
 		 }
-		return true;
+		return null;
 	}
 
 	@Override
-	public Boolean visit(This thisExpression, Object context)
+	public Object visit(This thisExpression)
 			throws SemanticError {
+		
 		if( inside_static_function == true)
 		{
-			String err_msg = "Found this in use not in an instance method";
+			String err_msg = "this keyword can only be used inside a virtual method";
 			throw new SemanticError(thisExpression .getLine(), err_msg);
 		}
-		else
-		{
-			return true;
-		}
+		
+		
+		return null;
+		
 	
 	}
 
 	@Override
-	public Boolean visit(NewClass newClass, Object context)
+	public Object visit(NewClass newClass)
 			throws SemanticError {
 
-		return true;
+		return null;
 	}
 
 	@Override
-	public Boolean visit(NewArray newArray, Object context)
-			throws SemanticError {
-		newArray.getSize().accept(this,null);
-		return true;
-	}
-
-	@Override
-	public Boolean visit(Length length, Object context) throws SemanticError {
-		length.getArray().accept(this,null);
-		return true;
-	}
-
-	@Override
-	public Boolean visit(MathBinaryOp binaryOp, Object context)
+	public Object visit(NewArray newArray)
 			throws SemanticError {
 		
-		binaryOp.getFirstOperand().accept(this,null);
-		binaryOp.getSecondOperand().accept(this,null);
+		newArray.getSize().accept(this);
+		
+		return null;
+	}
+
+	@Override
+	public Object visit(Length length) throws SemanticError {
+		
+		length.getArray().accept(this);
+		
+		return null;
+	}
+
+	@Override
+	public Object visit(MathBinaryOp binaryOp)
+			throws SemanticError {
+		
+		binaryOp.getFirstOperand().accept(this);
+		binaryOp.getSecondOperand().accept(this);
 	
-		return true;
+		return null;
 	}
 
 	@Override
-	public Boolean visit(LogicalBinaryOp binaryOp, Object context)
+	public Object visit(LogicalBinaryOp binaryOp)
 			throws SemanticError {
-		binaryOp.getFirstOperand().accept(this,null);
-		binaryOp.getSecondOperand().accept(this,null);
-		return true;
+		binaryOp.getFirstOperand().accept(this);
+		binaryOp.getSecondOperand().accept(this);
+		
+		return null;
 	}
 
 	@Override
-	public Boolean visit(MathUnaryOp unaryOp, Object context)
+	public Object visit(MathUnaryOp unaryOp)
 			throws SemanticError {
-		unaryOp.getOperand().accept(this,null);
-		return true;
+		
+		unaryOp.getOperand().accept(this);
+		return null;
 	}
 
 	@Override
-	public Boolean visit(LogicalUnaryOp unaryOp, Object context)
+	public Object visit(LogicalUnaryOp unaryOp)
 			throws SemanticError {
-		unaryOp.getOperand().accept(this,null);
-		return true;
+		
+		unaryOp.getOperand().accept(this);
+		
+		return null;
 	}
 
 	@Override
-	public Boolean visit(Literal literal, Object context) throws SemanticError {
-		return true;
+	public Object visit(Literal literal) throws SemanticError {
+		
+		return null;
 	}
 
 	@Override
-	public Boolean visit(ExpressionBlock expressionBlock, Object context)
+	public Object visit(ExpressionBlock expressionBlock)
 			throws SemanticError {
-		expressionBlock.getExpression().accept(this, null);
+		
+		expressionBlock.getExpression().accept(this);
 		
 		
-		return true;
+		return null;
 	}
 
 }
